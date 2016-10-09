786369c548e1f91ef2984a8bfcb4356dd01d54b1
Sungchul Choi <sc82.choi@gachon.ac.kr>
2016-10-09 23:17:28 +0900

    Update js codes

---
+++ web_programming/CS50_2016_web_programming/code/js/basic/boolean.js
@@ -0,0 +1,10 @@
+a = true;
+b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+console.log(150 > 145);
+console.log(1 == a);
+console.log(1 === a);
+console.log("" == b);
---
+++ web_programming/CS50_2016_web_programming/code/js/basic/string.js
@@ -0,0 +1,5 @@
+text_1 = "Hello, World";
+console.log(text_1.length);
+console.log(text_1[0]);
+console.log(text_1.slice(0,3));
+console.log(text_1.toUpperCase());
